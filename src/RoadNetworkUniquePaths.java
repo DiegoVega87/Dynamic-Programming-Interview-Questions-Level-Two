734c421a5d95a4e4a82019139c3119649d1bdb20
Diego Vega <diegovega@me.com>
2023-08-11 11:45:26 +0200

    Add problem explanation

---
+++ src/RoadNetworkUniquePaths.java
@@ -0,0 +1,11 @@
+public class RoadNetworkUniquePaths {
+
+    /*
+    * As a startup built to solve the problem of efficient commuting for city dwellers, we need to find the number of
+    * unique paths between two intersections in a given road network. We have the number of intersections and the
+    * number of roads connecting these intersections. As a commuter, you can only travel in the forward direction along
+    * the roads. Given this information, write a function that returns the number of unique paths from the starting
+    * intersection to the destination intersection.
+    *
+    * */
+}

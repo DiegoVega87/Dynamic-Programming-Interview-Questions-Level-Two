917f1c7ed0e5a76a7c07da93b62b1824d42ea5c2
Diego Vega <diegovega@me.com>
2023-08-12 14:04:26 +0200

    Add problem explanation

---
+++ src/EfficientCashRegister.java
@@ -0,0 +1,11 @@
+public class EfficientCashRegister {
+
+    /*
+    * As a startup, we are developing a cash register system that can make the fewest number of coins for any amount
+    * of money. Given an array coins that stores all available coins (1ct, 2ct etc...)  and the price amount, return the
+    * smallest number of coins and bills possible to pay the price. Return -1 if the given coins are not suitable to pay
+    * the exact price.
+    *
+    *
+    * */
+}

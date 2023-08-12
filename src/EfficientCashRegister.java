2ff8e5fc7ffeeade8d6fa0147de195a74f843dae
Diego Vega <diegovega@me.com>
2023-08-12 14:37:31 +0200

    Add recursive solution

--- src/EfficientCashRegister.java
+++ src/EfficientCashRegister.java
@@ -1,3 +1,5 @@
+import javax.naming.PartialResultException;
+
 public class EfficientCashRegister {
 
     /*
@@ -18,4 +20,37 @@ public class EfficientCashRegister {
     * int expectedOutput = 9;
     *
     * */
+    public static void main(String[] args){
+
+        int[] coins = {1, 5, 10, 25};
+        int price = 76;
+        System.out.println(calculateMinCoins(coins,price));
+    }
+    public static int calculateMinCoins(int[] coins, int price){
+
+        int n = coins.length;
+
+        return calculateMinCoins(coins, n-1, price);
+    }
+
+    private static int calculateMinCoins(int[] coins, int index, int target){
+
+
+        if(index == 0){
+            if(target % coins[0] == 0){
+                return target/coins[0];
+            }else{
+                return -1;
+            }
+        }
+
+        int notTake = calculateMinCoins(coins, index - 1, target);
+        int take = Integer.MAX_VALUE;
+
+        if(coins[index] <= target){
+            take =  1 + calculateMinCoins(coins, index, target - coins[index]);
+        }
+
+        return Math.min(take, notTake);
+    }
 }

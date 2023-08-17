8ae3b7c6a49272bbfc046cd2cd105ad7398a6833
Diego Vega <diegovega@me.com>
2023-08-17 16:54:13 +0200

    Add problem expanation

---
+++ src/SalesPerformance.java
@@ -0,0 +1,9 @@
+public class SalesPerformance {
+
+    /*
+    * As a growing startup, we want to analyze the performance of our sales team. To do this, we need to find the length
+    * of the longest increasing sequence of our monthly sales data stored in an integer array. Write a function that
+    * takes in the array and returns the length of the longest increasing subsequence. This will help us identify the
+    * salesperson with the highest sales growth rate and improve our overall sales performance.
+    * */
+}

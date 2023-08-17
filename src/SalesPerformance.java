b93dc9aca1ab65317f8ac8ac1d82080e05e31f31
Diego Vega <diegovega@me.com>
2023-08-17 17:01:04 +0200

    Add examples

--- src/SalesPerformance.java
+++ src/SalesPerformance.java
@@ -5,5 +5,15 @@ public class SalesPerformance {
     * of the longest increasing sequence of our monthly sales data stored in an integer array. Write a function that
     * takes in the array and returns the length of the longest increasing subsequence. This will help us identify the
     * salesperson with the highest sales growth rate and improve our overall sales performance.
+    *
+    * Example 1:
+    * int[] salesData = {1, 3, 2, 4, 6, 5, 8, 7, 9};
+    * int result = longestIncreasingSubsequence(salesData);
+    * System.out.println(result); // Output: 6, )
+    *
+    * Example 2:
+    * int[] salesData = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+    * int result = longestIncreasingSubsequence(salesData);
+    * System.out.println(result); // Output: 1
     * */
 }
